038019b851225976adb65dcfb4c81f437402dac0
aadar <aadar@DESKTOP-3K7GUL9>
2023-12-25 00:31:11 +0530

    updated

---
+++ JayshreeMaam/src/com/testprograms5/PlayersList.java
@@ -0,0 +1,27 @@
+package com.testprograms5;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class PlayersList {
+	
+
+	
+	public static void main(String args[])
+	{
+		List<Players> list= new ArrayList<>();
+		list.add(new Players(1,"Aadarsh","India","Chennai SuperKings"));
+		list.add(new Players(2,"Jayraj","Pakistan","Kochi SuperKings"));
+		list.add(new Players(3,"Rohit","India","Banglore SuperKings"));
+		list.add(new Players(4,"Abhishek","Australia","Hyderabad SuperKings"));
+		list.add(new Players(5,"Sudharshan","Australia","Pune SuperKings"));
+		list.add(new Players(6,"Suraj","Australia","Pune SuperKings"));
+		list.add(new Players(7,"Jitu","India","Pune SuperKings"));
+		list.add(new Players(8,"Sidharth","India","Punjab SuperKings"));
+		
+		
+		
+	}
+
+}
