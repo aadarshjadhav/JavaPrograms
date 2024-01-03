e01bbf0d3888f6b8f70a071c3fdc6125cd5475f3
aadar <aadar@DESKTOP-3K7GUL9>
2024-01-03 15:32:49 +0530

    updated

--- JayshreeMaam/src/com/finaltestpractices/CityRainFall.java
+++ JayshreeMaam/src/com/finaltestpractices/CityRainFall.java
@@ -1,5 +1,7 @@
 package com.finaltestpractices;
 
+import java.util.Objects;
+
 class CityRainFall {
     String cityname;
     int raininjul;
@@ -11,4 +13,29 @@ class CityRainFall {
         this.raininjul = raininjul;
         this.raininaug = raininaug;
     }
+    
+    
+
+	@Override
+	public String toString() {
+		return "CityRainFall [cityname=" + cityname + ", raininjul=" + raininjul + ", raininaug=" + raininaug + "]";
+	}
+
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(cityname);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		CityRainFall obj_city=(CityRainFall) obj;
+		if(Objects.equals(cityname, obj_city.cityname))
+			return true;
+		else
+		return  false;
+	}
+    
+    
 }
--- JayshreeMaam/src/com/finaltestpractices/MainCityRainFall.java
+++ JayshreeMaam/src/com/finaltestpractices/MainCityRainFall.java
@@ -2,10 +2,12 @@ package com.finaltestpractices;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Iterator;
 
 public class MainCityRainFall {
 
 	public static void main(String[] args) {
+		
 		
 		ArrayList<CityRainFall> list_july_only= new ArrayList<>();
 		list_july_only.add(new CityRainFall ("Pune",25,0));
@@ -20,6 +22,54 @@ public class MainCityRainFall {
 		combineRecords(combined_hashset,list_july_only);
 		combineRecords(combined_hashset,list_august_only);
 		
-	}
+		Iterator<CityRainFall> itr_combined_hashset=combined_hashset.iterator();
+		
+		while(itr_combined_hashset.hasNext())
+		{
+			CityRainFall obj_city_rain_fall=itr_combined_hashset.next();
+			System.out.println(obj_city_rain_fall);
+		}
+	}	
+	
+	public static void combineRecords(HashSet<CityRainFall> set, ArrayList<CityRainFall> list)
+	{
+		
+		System.out.println("Inside combineRecords Function:");
+		Iterator<CityRainFall> itr_list = list.iterator();
+		
+		while(itr_list.hasNext())
+		{
+			System.out.println("Inside While loop:");
+	        CityRainFall obj_cityfall = itr_list.next();
 
+	        // Check if the set already contains the city
+	        boolean cityExists = false;
+	        CityRainFall existingRecord = null;
+	        
+	        //Using for loop to check if the hashset already has the object city from the list
+	        for (CityRainFall record : set) 
+	        {
+	            if (record.equals(obj_cityfall)) 
+	            {
+	                cityExists = true;
+	                existingRecord = record;
+	                break;
+	            }
+	        }
+
+	        // Combine the rainfall values if the city exists
+	        if (cityExists) 
+	        {
+	            existingRecord.raininjul += obj_cityfall.raininjul;
+	            existingRecord.raininaug += obj_cityfall.raininaug;
+	        } 
+	        else 
+	        {
+	            // Add the new record to the set
+	            set.add(obj_cityfall);
+	            System.out.println("Output of obj_cityfall:");
+	            System.out.println(obj_cityfall);
+	        }
+		}
+	}
 }
